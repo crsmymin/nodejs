6f44a66766dea2a60bc74d9c74fae25b8ccec3f9
yeongminkang <yeongmin.kang@cfind.co.kr>
2021-09-07 15:07:48 +0900

    팝업생성

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,43 @@
-// import Swiper JS
-import Swiper from 'swiper';
+import "../lib/jquery-ui.min";
 
-// import Swiper styles
-import 'swiper/css';
+var css_z_idx = 99;
 
-$(function () {
-  // 메인 페이지 배너 슬라이드
-  const swiper = new Swiper('.slide-container', {
-    // Optional parameters
-    slidesPerView: 1,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false
-    },
-    loop: true,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+$(".popup").draggable()
+.css( 'cursor', 'move' )
+.mousedown(function(){ // mousedown 이벤트 생성
+    $(this).css('z-index', css_z_idx); // 클릭한 이미지만 z-index 증가시킴
+    css_z_idx++;
+});
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+$(".popup").each(function(index, item) {
+  var popupNo = index + 1;
+  $(".popup" + popupNo).css({
+    left : 350 * index
+  })
+})
 
+function setCookie(name, value, expiredays) {
+  var todayDate = new Date();
+  todayDate.setDate(todayDate.getDate() + expiredays);
+  document.cookie = name + "=" + escape(value) + "; path=/; expires=" + todayDate.toGMTString() + ";"
+}
+
+function getCookie() {
+  var cookiedata = document.cookie;
+  if (cookiedata.indexOf("todayCookie=done") < 0) {
+    $(".popup").show();
+  } else {
+    $(".popup").hide();
+  }
+}
+
+getCookie();
+
+$(".popup-close").on("click", function() {
+  $(this).parents(".popup").hide();
+})
+
+$(".today-popup-close").on("click", function() {
+  setCookie("todayCookie", "done", 1);
+  $(this).parents(".popup").hide();
 })
